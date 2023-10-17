b003d8961ee35a492487b4cf6818529783a7d8be
etkaesque <edga.majo@gmail.com>
2023-10-17 20:17:05 +0300

    setting up

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import vue from "@vitejs/plugin-vue2";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "https://etkaesque.github.io/contacts/",
   test: {
     globals: true,
     environment: "jsdom",
   },
   define: {
     SERVER_ADDR: JSON.stringify('https://contactspb.hop.sh'),
-    STRUCTURES_PER_PAGE:8
+    STRUCTURES_PER_PAGE:8,
+
 
   }
 });
