3208cf9c17c51b48c279f415c47ceb5e2e7a0399
etkaesque <edga.majo@gmail.com>
2023-10-17 20:40:16 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue2";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "https://etkaesque.github.io/contacts/",
   test: {
     globals: true,
     environment: "jsdom",
